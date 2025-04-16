c61f71b88c22a647d7741b7423bce310aeee7aca
Matheus Cavasini <cavasini.dev@gmail.com>
2025-04-16 00:51:54 -0300

    alterando yaml

--- src/main/java/com/timewise/AppointmentService/dto/AppointmentPostRequest.java
+++ src/main/java/com/timewise/AppointmentService/dto/AppointmentPostRequest.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record AppointmentPostRequest(
-//        UUID userId,
+        UUID userId,
         UUID clientId,
         LocalDateTime startTime,
         LocalDateTime endTime,
