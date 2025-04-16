b93d2c376c80bbd970801646b13ebfe3fa1037b7
Matheus Cavasini <cavasini.dev@gmail.com>
2025-04-16 00:55:31 -0300

    alterando yaml

--- src/main/java/com/timewise/AppointmentService/controller/AppointmentController.java
+++ src/main/java/com/timewise/AppointmentService/controller/AppointmentController.java
@@ -20,40 +20,45 @@ import java.util.Optional;
 @RequestMapping("/api/v1/appointments")
 public class AppointmentController {
 
-    @Autowired
-    private AppointmentService appointmentService;
+//    @Autowired
+//    private AppointmentService appointmentService;
+//
+//    @GetMapping("/{userId}")
+//    public ResponseEntity<Page<Appointment>> getAppointmentsByUser(@PathVariable("userId") String userId, @PageableDefault(size = 10, direction = Sort.Direction.ASC) Pageable pageable){
+//        Page<Appointment> response = appointmentService.getAllAppointmentsByUserId(userId, pageable);
+//        return ResponseEntity.ok(response);
+//    }
+//
+//    @GetMapping("/appo/{id}")
+//    public ResponseEntity getAppointmentById(
+//            @PathVariable("id") String id
+//    ){
+//        Optional<Appointment> response = appointmentService.getAppointmentById(id);
+//        if(response.isPresent()){
+//            return ResponseEntity.ok(response);
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity addAppointment(@RequestBody AppointmentPostRequest appointmentPostRequest) {
+//        AppointmentPostResponse response = null;
+//        try {
+//            response = appointmentService.createAppointment(appointmentPostRequest);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//        return ResponseEntity.ok(response);
+//    }
+//
+//    @GetMapping("teste")
+//    public ScheduleAvailabilityResponse teste(){
+//        return appointmentService.getappo();
+//    }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<Page<Appointment>> getAppointmentsByUser(@PathVariable("userId") String userId, @PageableDefault(size = 10, direction = Sort.Direction.ASC) Pageable pageable){
-        Page<Appointment> response = appointmentService.getAllAppointmentsByUserId(userId, pageable);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/appo/{id}")
-    public ResponseEntity getAppointmentById(
-            @PathVariable("id") String id
-    ){
-        Optional<Appointment> response = appointmentService.getAppointmentById(id);
-        if(response.isPresent()){
-            return ResponseEntity.ok(response);
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    @PostMapping
-    public ResponseEntity addAppointment(@RequestBody AppointmentPostRequest appointmentPostRequest) {
-        AppointmentPostResponse response = null;
-        try {
-            response = appointmentService.createAppointment(appointmentPostRequest);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("teste")
-    public ScheduleAvailabilityResponse teste(){
-        return appointmentService.getappo();
+    @GetMapping
+    public ResponseEntity<String> getTest(){
+        return ResponseEntity.ok("deu bom");
     }
 
 }
